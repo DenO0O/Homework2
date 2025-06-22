2608313bcf802f39133b554f1ea85c35af63e4ac
Денис <deny001@yandex.ru>
2025-06-22 16:52:50 +0500

    Задание 1

---
+++ HW2_1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace HW2_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int ClickCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CTR_Clicks.Text = ClickCount.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClickCount++;
+            CTR_Clicks.Text = ClickCount.ToString();
+        }
+    }
+}
