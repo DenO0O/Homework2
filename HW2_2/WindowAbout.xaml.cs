c8035a85e1a97ef5a677bbaa19978d0d41149a8f
Денис <deny001@yandex.ru>
2025-06-28 20:34:47 +0500

    Задание 2

---
+++ HW2_2/WindowAbout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HW2_2
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAbout.xaml
+    /// </summary>
+    public partial class WindowAbout : Window
+    {
+        public WindowAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
